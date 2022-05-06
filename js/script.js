dac2377337963bf07d15353bcc9133a26f4cd664
Robson Rangel <rangell.robson@gmail.com>
2022-05-06 19:01:38 -0300

    atualização codigos

--- js/script.js
+++ js/script.js
@@ -3,3 +3,40 @@ window.addEventListener("scroll", function () {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const seconds = document.getElementById('seconds');
+const minutes = document.getElementById('minutes');
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+}
+
+setInterval(updateCountdown, 1000);
+
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
